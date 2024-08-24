a14444e0d58213176515a9818ec7228f6eff179d
SafariOl <s.olha.vit@gmail.com>
2024-08-24 17:36:16 +0300

    removed function

--- task_2/main.py
+++ task_2/main.py
@@ -1,11 +1,4 @@
 from mod import calculation
 
-def product(x, y):
-    dob = 1
-    for i in range(x, y + 1):
-        if i % 2 == 1:
-            dob *= i
-    return dob
-
 n = int(input("Введіть значення n: "))
-print(f"Сума числа n={n}:", calculation(n))
+print(f"Сума числа n: {calculation(n):.2f}:")
