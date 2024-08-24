59224fba946b3ab51dce217bc7f5bd893b4635a9
SafariOl <s.olha.vit@gmail.com>
2024-08-24 17:35:28 +0300

    removed function

--- task_2/main.py
+++ task_2/main.py
@@ -1,17 +1,11 @@
-import math
 from mod import calculation
 
-def expression(a, b):
-    z = 0
-    if a >= 15 :
-        z = math.sin(2*a) + math.cos(2*b)
-    else:
-        z = math.sqrt(a + b**2)
-    return z
-
-a = int(input("Введіть значення a: "))
-b = int(input("Введіть значення b: "))
-print(f"Значення виразу z = {expression(a, b):.2f}")
+def product(x, y):
+    dob = 1
+    for i in range(x, y + 1):
+        if i % 2 == 1:
+            dob *= i
+    return dob
 
 n = int(input("Введіть значення n: "))
-print(f"Сума числа n: {calculation(n):.2f}")
+print(f"Сума числа n={n}:", calculation(n))
